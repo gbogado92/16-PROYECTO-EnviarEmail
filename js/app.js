a6eb75dead37f5906abbe009835814cda3e08cbf
gbogado92 <62188600+gbogado92@users.noreply.github.com>
2021-05-05 16:35:06 -0300

    Validar los campos

--- js/app.js
+++ js/app.js
@@ -3,9 +3,20 @@
 //  Variables 
 const BtnEnviar = document.querySelector('#enviar');
 
+// Variables para campos 
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
 eventListeners();
 function eventListeners() {
+    //  Cuando la app arranca 
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    //  Campos del formulario 
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 //  Funciones 
@@ -13,4 +24,13 @@ function eventListeners() {
 function iniciarApp() {
    BtnEnviar.disabled = true;
    BtnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+//  Valida el formulario 
+function validarFormulario(e) {
+    if(e.target.value.length > 0) {
+        console.log('Si hay algo');
+    } else {
+        e.target.classList.add('border', 'border-red-500');
+    }
 }
