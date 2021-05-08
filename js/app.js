a9681d28c9938ec1cc2630f13e601d9e4a9799e2
gbogado92 <62188600+gbogado92@users.noreply.github.com>
2021-05-08 12:02:08 -0300

    Reordena nuestro código para que sea más fácil de mantener

--- js/app.js
+++ js/app.js
@@ -9,6 +9,8 @@ const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 eventListeners();
 function eventListeners() {
     //  Cuando la app arranca 
@@ -33,7 +35,10 @@ function validarFormulario(e) {
 
         //  Elimina los errores... 
         const error = document.querySelector('p.error');
-        error.remove();
+        if (error) {
+            error.remove();
+        }
+
 
         e.target.classList.remove('border', 'border-red-500');
         e.target.classList.add('border', 'border-green-500');
@@ -46,11 +51,11 @@ function validarFormulario(e) {
 
     if (e.target.type === 'email') {
 
-        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
         if( er.test( e.target.value ) ) {
             const error = document.querySelector('p.error');
-            error.remove();
+            if (error) {
+                error.remove();
+            }
     
             e.target.classList.remove('border', 'border-red-500');
             e.target.classList.add('border', 'border-green-500');
@@ -59,6 +64,11 @@ function validarFormulario(e) {
             e.target.classList.add('border', 'border-red-500');        
             mostrarError('Email no válido');
         }
+    }
+
+    if ( er.test( email.value ) && asunto.value !== '' && mensaje.value !== '' ) {
+        BtnEnviar.disabled = false;
+        BtnEnviar.classList.remove('cursor-not-allowed', 'opacity-50');
     }
 }
 
