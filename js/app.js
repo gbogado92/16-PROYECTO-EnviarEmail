305d315b541e8ef736c73a4e08c52dddf2201704
Gabriel Bogado <gabrielgbogado92@gmail.com>
2021-05-09 17:45:56 -0300

    Guardando cambios locales para fusionar

--- js/app.js
+++ js/app.js
@@ -4,8 +4,12 @@
 const BtnEnviar = document.querySelector('#enviar');
 
 eventListeners();
+//  Cuando la app arranca 
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', iniciarApp);
+
+    //  Campos del formulario 
+    email.addEventListener('blur', validarFormulario);
 }
 
 //  Funciones 
@@ -13,4 +17,12 @@ function eventListeners() {
 function iniciarApp() {
    BtnEnviar.disabled = true;
    BtnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+//  Valida el formulario 
+function validarFormulario(e) {
+    if(e.target.value.length) {
+        console.log('Si hay algo');
+    } else {
+        e.target.style.borderBottomColor = 'red';
+    }
 }
